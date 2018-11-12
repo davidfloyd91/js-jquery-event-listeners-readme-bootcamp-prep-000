d8e5ca9ffd292ac1a19c4cf70c7342e71fc77267
David Floyd <davidfloyd91@gmail.com>
2018-11-12 00:12:31 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -1,13 +1,23 @@
 function getIt() {
   $('p').on('click', function() {
     alert('Hey!')
-  })
+  });
 }
 
 function frameIt() {
   $('img').on('load', function() {
     $('img').classList.add('tasty')
 });
+
+function pressIt() {
+  $('form').on('keydown', function(key) {
+    if key = 71 {
+      alert('You\'ve pressed G!')
+    }
+  });
+}
+
+
 
 $(document).ready(function(){
 
